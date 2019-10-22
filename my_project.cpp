73034736336406333761dbdf1713422da494f3dc
691171501 <xuchaowu@localhost.localdomain>
2019-10-22 03:35:01 -0700

    first

---
+++ my_project.cpp
@@ -0,0 +1,9 @@
+//
+// Created by xuchaowu on 10/22/19.
+//
+
+#include "my_project.h"
+
+int main() {
+    return  0;
+}
---
+++ my_project.h
@@ -0,0 +1,14 @@
+//
+// Created by xuchaowu on 10/22/19.
+//
+
+#ifndef TEST_MY_PROJECT_H
+#define TEST_MY_PROJECT_H
+
+
+class my_project {
+
+};
+
+
+#endif //TEST_MY_PROJECT_H
